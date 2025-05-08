c037160eae3d41a7eb685cd64488031144372321
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-05-08 23:30:14 +0900

    add `Idb.open` and `Idb.delete`

--- src/idb.ts
+++ src/idb.ts
@@ -1,10 +1,21 @@
 import type { NormalizeObject } from "@uzmoi/ut/types";
 import { IdbTransaction } from "./transaction.ts";
 import type { IdbTransactionModeMap, IdbType } from "./types.ts";
+import { requestToPromise } from "./utils.ts";
 
 export class Idb<out T>
   implements
     IdbType<Omit<IDBDatabase, "createObjectStore" | "deleteObjectStore">> {
+  static async open<T>(name: string, version: number): Promise<Idb<T>> {
+    const req = indexedDB.open(name, version);
+    return new Idb(await requestToPromise(req));
+  }
+
+  static async delete(name: string): Promise<void> {
+    const req = indexedDB.deleteDatabase(name);
+    await requestToPromise(req);
+  }
+
   /** @ignore */
   private constructor(private readonly db: IDBDatabase) {}
 
